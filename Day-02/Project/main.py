e6504df6191a4bafb6e47563911196d71c9ff62e
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2026-01-04 19:44:19 +0700

    Update and rename New Python File.py to main.py

---
+++ Day-02/Project/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+
+total = bill + (bill/100)* tip
+bill_per_person = round(total / people, 2)
+print(f'Each person should pay: ${bill_per_person}')
